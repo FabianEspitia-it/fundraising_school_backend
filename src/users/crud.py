38fe44cb1767f299342ef5f27dfbbd225b3eb5e6
Fabián Espitia <fabianespitia370@gmail.com>
2024-07-21 14:56:08 -0500

    fixed startup bug

--- src/users/crud.py
+++ src/users/crud.py
@@ -135,21 +135,12 @@ def get_favorite_startups_by_user_id(db: Session, id: int) -> list[dict]:
         joinedload(models.Startup.round)
     ).all()
 
-    startups = []
-    for startup in query:
-        startup_dict = {
-            column: getattr(startup, column)
-            for column in models.Startup.__table__.columns.keys()
-            if column not in ['id', 'photo', 'country_id', 'traction_id', 'sector_id', 'round_id']
-        }
-        startup_dict['country'] = startup.country.name if startup.country else None
-        startup_dict['traction'] = startup.traction.name if startup.traction else None
-        startup_dict['sector'] = startup.sector.name if startup.sector else None
-        startup_dict['round'] = startup.round.name if startup.round else None
+    
 
-        startups.append(startup_dict)
+    return query
 
-    return startups
+
+
 
 
 def delete_favorite_startup_by_user_email(db: Session, email: str, startup_id: int) -> None:
