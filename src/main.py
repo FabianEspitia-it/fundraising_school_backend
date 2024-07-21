5ab26eef67d8bfd73d9d2b1f9090fd9716f1a5de
Fabián Espitia <fabianespitia370@gmail.com>
2024-07-21 18:50:53 -0500

    api key added

--- src/main.py
+++ src/main.py
@@ -1,15 +1,26 @@
 import os
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 
 from src.course.router import course
 from src.users.router import user
 from src.vc_sheet.router import vc_sheet_router
 from src.startups.router import startup_router
+from fastapi.security import OAuth2PasswordBearer
 
 
-app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key != os.getenv("API_KEY"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden"
+        )
+
+app = FastAPI(dependencies=[Depends(api_key_auth)])
+
 
 app.title = "Fundraising School API"
 
