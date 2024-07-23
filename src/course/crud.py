5b4a9cceb7dbe3c4298da419a021d3c9b11bee49
Sergio Rey <reysergio383@gmail.com>
2024-07-23 00:07:38 -0500

    refactor: Update user_email parameter type in add_class_as_seen function

--- src/course/crud.py
+++ src/course/crud.py
@@ -100,8 +100,8 @@ def get_course_by_name_method(db: Session, course_name: str):
     return db.query(Course).filter(Course.title == course_name).first()
 
 
-def add_class_as_seen(db: Session, class_id: int, user_email: UserEmail):
-    user_db = db.query(User).filter(User.email == user_email.email).options(joinedload(User.classes)).first()
+def add_class_as_seen(db: Session, class_id: int, user_email: str):
+    user_db = db.query(User).filter(User.email == user_email).options(joinedload(User.classes)).first()
     class_db = db.query(Class).filter(Class.id == class_id).first()
 
     if user_db is None:
--- src/course/router.py
+++ src/course/router.py
@@ -40,7 +40,7 @@ def add_new_class_module(course_id: int, module_id: int, new_class: NewClass, db
     add_class_to_module(db, course_id, module_id, new_class)
 
 
-@course.get("/course/class/{class_name}", tags= ["courses"])
+@course.get("/course/class/{class_name}", tags=["courses"])
 def get_class_by_name(class_name: str, db: Session = Depends(get_db)):
     class_instance = get_class_by_name_method(db, class_name)
     if class_instance is None:
@@ -48,7 +48,7 @@ def get_class_by_name(class_name: str, db: Session = Depends(get_db)):
     return class_instance
 
 
-@course.get("/course/name/{course_name}", tags= ["courses"])
+@course.get("/course/name/{course_name}", tags=["courses"])
 def get_course_by_name(course_name: str, db: Session = Depends(get_db)):
     course = get_course_by_name_method(db, course_name)
     if course is None:
@@ -56,17 +56,17 @@ def get_course_by_name(course_name: str, db: Session = Depends(get_db)):
     return course
 
 
-@course.post("/course/{course_name}/modules/{module_id}/classes/{class_id}/completed", tags=['courses'])
-def mark_class_complete(class_id: int, user_email: UserEmail, db: Session = Depends(get_db)):
+@course.post("/course/classes/{class_id}/user/{user_email}/completed", tags=['courses'])
+def mark_class_complete(class_id: int, user_email: str, db: Session = Depends(get_db)):
     add_class_as_seen(db, class_id, user_email)
     return JSONResponse(status_code=201, content={'response': 'Class completed'})
 
 
-@course.get('/course/{course_name}/module/{module_id}/classes/{class_id}/next_class', tags=['courses'])
+@course.get('/course/module/{module_id}/classes/{class_id}/next', tags=['courses'])
 def get_next_class(module_id: int, class_id: int, db: Session = Depends(get_db)):
     return get_next_class_course(db, class_id, module_id)
 
 
-@course.get('/course/{course_name}/module/{module_id}/classes/{class_id}/previous_class', tags=['courses'])
+@course.get('/course/module/{module_id}/classes/{class_id}/prev', tags=['courses'])
 def get_next_class(module_id: int, class_id: int, db: Session = Depends(get_db)):
     return get_previous_class_course(db, class_id, module_id)
