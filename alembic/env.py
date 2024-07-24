4b51e950a1b17a801166308dc4a68923ede6edac
PoliSan123 <50530255+PoliSan123@users.noreply.github.com>
2024-07-24 16:35:59 -0500

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -16,7 +16,6 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
