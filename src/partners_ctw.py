a774a289937ece6642be26732f8a704aff8cecf7
Fabián Espitia <fabianespitia370@gmail.com>
2024-08-21 15:26:36 -0500

    partners script modified

--- src/partners_ctw.py
+++ src/partners_ctw.py
@@ -10,50 +10,54 @@ def process_excel(file_path: str, db: Session):
     print(df.columns)
 
     for _, row in df.iterrows():
-        partner = Partner(
-            name = row["Representante 1"] if pd.notna(row["Representante 1"]) else None,
-            photo = row["Foto"] if pd.notna(row["Foto"]) else None,
-            role = row["Cargo"] if pd.notna(row["Cargo"]) else None,
-            email = row["Correo"] if pd.notna(row["Correo"]) else None,
-            linkedin = row["LinkedIn"] if pd.notna(row["LinkedIn"]) else None,
+        if row["Batch"] == "S1":
+            if pd.notna(row["Representante 1 "]):
+                partner = Partner(
+                    name=row["Representante 1 "],
+                    photo=row["Foto"] if pd.notna(row["Foto"]) else None,
+                    role=row["Cargo"] if pd.notna(row["Cargo"]) else None,
+                    email=row["Correo"] if pd.notna(row["Correo"]) else None,
+                    linkedin=row["LinkedIn"] if pd.notna(row["LinkedIn"]) else None,
+                )
 
-        )
+                db.add(partner)
+                db.commit()
+                db.refresh(partner)
 
-        db.add(partner)
-        db.commit()
-        db.refresh(partner)
+                fund = db.query(Fund).filter(Fund.name == row["Nombre Fondo"]).first()
 
-        fund = db.query(Fund).filter(Fund.name == row["Nombre Fondo"]).first()
+                user_fund = FundPartner(
+                    fund_id=fund.id,
+                    partner_id=partner.id
+                )
 
-        user_fund = FundPartner(
-            fund_id = fund.id,
-            partner_id = partner.id
-        )
+                db.add(user_fund)
+                db.commit()
+                db.refresh(user_fund)
 
-        db.add(user_fund)
-        db.commit()
-        db.refresh(user_fund)
+            
+            if pd.notna(row["Representante 2"]):
+                partner_2 = Partner(
+                    name=row["Representante 2"],
+                    photo=row["Foto 2"] if pd.notna(row["Foto 2"]) else None,
+                    role=row["Cargo 2"] if pd.notna(row["Cargo 2"]) else None,
+                    email=row["Correo 2"] if pd.notna(row["Correo 2"]) else None,
+                    linkedin=row["LinkedIn 2"] if pd.notna(row["LinkedIn 2"]) else None,
+                )
 
-        partern_2 = Partner(
-            name = row["Representante 2"] if pd.notna(row["Representante 2"]) else None,
-            photo = row["Foto 2"] if pd.notna(row["Foto 2"]) else None,
-            role = row["Cargo 2"] if pd.notna(row["Cargo 2"]) else None,
-            email = row["Correo 2"] if pd.notna(row["Correo 2"]) else None,
-            linkedin = row["LinkedIn 2"] if pd.notna(row["LinkedIn 2"]) else None,
+                db.add(partner_2)
+                db.commit()
+                db.refresh(partner_2)
 
-        )
+                user_fund_two = FundPartner(
+                    fund_id=fund.id,
+                    partner_id=partner_2.id
+                )
 
-        db.add(partern_2)
-        db.commit()
-        db.refresh(partern_2)
-
-        user_fund_two = FundPartner(
-            fund_id = fund.id,
-            partner_id = partern_2.id
-        )
-
-        db.add(user_fund_two)
-        db.commit()
-        db.refresh(user_fund_two)
-
+                db.add(user_fund_two)
+                db.commit()
+                db.refresh(user_fund_two)
         
+
+db_session = next(get_db())  
+process_excel("src/partners_ctw.xlsx", db_session)
